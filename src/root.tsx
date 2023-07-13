2d8702801f83f0dfb02ce36b4baeb0b001f94bef
p32929 <p32929@yahoo.com>
2023-07-13 06:32:30 +0600

    added react tailwind

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,11 @@
 import * as ReactDOM from 'react-dom';
 
 function render() {
-    ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+    ReactDOM.render(
+        <div className="md:container mx-auto flex items-center justify-center h-screen bg-gray-200">
+            <p className=" text-indigo-400 text-2xl">React + Tailwind + Typescript + Electron = ❤</p>
+        </div>
+        , document.body);
 }
 
 render();
