da32358f14e686db3b87b964e37ed69ebaa863c6
p32929 <p32929@yahoo.com>
2023-07-13 06:44:14 +0600

    adding shadcn

--- src/root.tsx
+++ src/root.tsx
@@ -2,9 +2,9 @@ import * as ReactDOM from 'react-dom';
 
 function render() {
     ReactDOM.render(
-        <div className="md:container mx-auto flex items-center justify-center h-screen bg-gray-200">
-            <p className=" text-indigo-400 text-2xl">React + Tailwind + Typescript + Electron = ❤</p>
-        </div>
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+            Taxing Laughter: The Joke Tax Chronicles
+        </h1>
         , document.body);
 }
 
