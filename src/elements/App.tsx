dd23c5c410050ee32967afda73e3fe88d6f563df
p32929 <p32929@yahoo.com>
2023-08-08 04:09:28 +0600

    overmind added

--- src/elements/App.tsx
+++ src/elements/App.tsx
@@ -1,25 +1,18 @@
+
+import { useActions, useAppState } from '@/overmind/OvermindHelper'
 import React from 'react'
-import { Button } from "@/components/ui/button";
 
 interface Props {
 
 }
 
 const App: React.FC<Props> = (props) => {
+    const actions = useActions()
+    const states = useAppState()
 
     return (
-        <div className='container text-center gap-4 space-x-4 space-y-4'>
-            {/* <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-                Counter: {states.counter}
-            </h1>
-
-            <Button onClick={() => {
-                controller.decrease()
-            }}>---</Button>
-
-            <Button onClick={() => {
-                controller.increase()
-            }}>+++</Button> */}
+        <div>
+            App
         </div>
     )
 
--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,18 @@
 import * as ReactDOM from 'react-dom';
 import App from './elements/App';
 import React from 'react';
+import { Provider } from "overmind-react";
+import { createOvermind } from 'overmind'
+import { config } from './overmind/OvermindHelper'
+
+const overmind = createOvermind(config)
 
 function render() {
     ReactDOM.render(
         <React.StrictMode>
-            <App />
+            <Provider value={overmind}>
+                <App />
+            </Provider>
         </React.StrictMode>
         , document.body);
 }
